aac09c56aafb914aa686ee6703c1e95aaed55469
tofayeLL <ahmedtofayel132@gmail.com>
2025-04-13 11:00:59 +0600

    finiahed breaking news section

--- src/pages/Home/BreakingNews/BreakingNews.jsx
+++ src/pages/Home/BreakingNews/BreakingNews.jsx
@@ -7,14 +7,14 @@ const BreakingNews = () => {
         <section>
                 
             <div className="flex items-center p-5 shadow-xl bg-slate-200 rounded-lg my-10">
-            <button className="btn bg-red-500 text-white">Breaking News</button>
+            <button className="btn bg-purple-500 text-white">Breaking News</button>
             <Marquee pauseOnHover={true} speed={140}>
                 <div className="space-x-10">
-                    <Link to={'/'}> I can be a React component, multiple React components, or just some text.</Link>
-                    <Link to={'/'}> I can be a React component, multiple React components, or just some text.</Link>
-                    <Link to={'/'}> I can be a React component, multiple React components, or just some text.</Link>
-                    <Link to={'/'}> I can be a React component, multiple React components, or just some text.</Link>
-                    <Link to={'/'}> I can be a React component, multiple React components, or just some text.</Link>
+                    <Link to={'/'}> Violent Protests Erupt in West Bengal Over Waqf Act</Link>
+                    <Link to={'/'}> India Showcases BioE3 Policy at Seoul Summit</Link>
+                    <Link to={'/'}> Mohamed Salah Signs Two-Year Extension with Liverpool</Link>
+                    <Link to={'/'}> Nationwide UPI Glitch Disrupts Digital Payments in India</Link>
+                    <Link to={'/'}> US-Iran to Hold Nuclear Talks in Oman</Link>
 
                 </div>
             </Marquee>
