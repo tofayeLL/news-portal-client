6a1529cb4f88de0a8ae9b7db497e4a1febe0b04e
tofayeLL <ahmedtofayel132@gmail.com>
2025-04-13 10:54:35 +0600

    finished breaking news section and added marquee

--- src/pages/Home/BreakingNews/BreakingNews.jsx
+++ src/pages/Home/BreakingNews/BreakingNews.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import Marquee from 'react-fast-marquee';
+import { Link } from 'react-router-dom';
 
 const BreakingNews = () => {
     return (
         <section>
-            <p>i am from breaking news</p>           
+                
+            <div className="flex items-center p-5 shadow-xl bg-slate-200 rounded-lg my-10">
+            <button className="btn bg-red-500 text-white">Breaking News</button>
+            <Marquee pauseOnHover={true} speed={140}>
+                <div className="space-x-10">
+                    <Link to={'/'}> I can be a React component, multiple React components, or just some text.</Link>
+                    <Link to={'/'}> I can be a React component, multiple React components, or just some text.</Link>
+                    <Link to={'/'}> I can be a React component, multiple React components, or just some text.</Link>
+                    <Link to={'/'}> I can be a React component, multiple React components, or just some text.</Link>
+                    <Link to={'/'}> I can be a React component, multiple React components, or just some text.</Link>
+
+                </div>
+            </Marquee>
+
+        </div>     
         </section>
     );
 };
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Banner from './Banner/Banner';
+import BreakingNews from './BreakingNews/BreakingNews';
 
 const Home = () => {
     return (
         <section className='py-24 lg:px-14'>
-            <p className=''>I am from home</p>
+        
+            <div className=''>
+                <BreakingNews></BreakingNews>
+            </div>
+
+
             {/* banner */}
             <Banner></Banner>
             
